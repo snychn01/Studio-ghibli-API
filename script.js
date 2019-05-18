576d4962e0e725ec43a425be4bee2073619a229a
sunny chouhan <snychn01@gmail.com>
2019-05-18 12:34:50 +0530

    add logo

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const container = document.createElement('div');
 //assign it a class name
 container.setAttribute('class', 'container');
 
-//placing it on the root dom element
-app.appendChild(container);
 
+const logo = document.createElement('img');
+
+logo.src = 'logo.png';
+//placing it on the root dom element
+app.appendChild(logo);
+
+app.appendChild(container);
 
 //creating a request var and assign it an object; to make an http request
 var request = new XMLHttpRequest();
 
-//open a connection specifiy http method and url endpoint
+//open a connection s qpecifiy http method and url endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 //access when request completes

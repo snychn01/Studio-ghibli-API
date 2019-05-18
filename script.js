83f2ac1ad4cad202daf61cca6065dbbcb138e910
sunny chouhan <snychn01@gmail.com>
2019-05-18 11:41:50 +0530

    Update script.js
    card added in container and h1 and p added in card

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//getting root element of DOM
+const app = document.getElementById('root');
+
+//creating new container element
+const container = document.createElement('div');
+//assign it a class name
+container.setAttribute('class', 'container');
+
+//placing it on the root dom element
+app.appendChild(container);
+
+
 //creating a request var and assign it an object; to make an http request
 var request = new XMLHttpRequest();
 
@@ -13,11 +25,29 @@ request.onload = function() {
   //if no error log data 
   if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
-      console.log(movie.title);
+      
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;	
+
+      container.appendChild(card)
+      card.appendChild(h1)
+      card.appendChild(p)
+    
     })
-  } else {
-    console.log('error');
-  }
+  } 
+
+  else {
+ 	const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+   }
 }
 
 //send the request
